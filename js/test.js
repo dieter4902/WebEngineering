5ef28444bc38d094d26fdb94b93dd2527865b6d7
adrian <s51037@bht-berlin.de>
2021-11-29 11:22:28 +0100

    ja aufgabe

--- js/test.js
+++ js/test.js
@@ -2,26 +2,27 @@
 //console.log("end me pls!");
 {
     "use strict";
-}
 
-function printPassword() {
-    const passwd = document.getElementById("password").value;
-    console.log(passwd);
-    checkPassword(passwd)
-}
-
-function checkPassword(password) {
-    if (password === null || typeof password === "undefined") {
-        alert("abgebrochen")
-    } else if (password === "") {
-        alert("leer");
-    } else if (password.length >= 8) {
-        alert("richtig");
-    } else {
-        alert("falsch")
+    function printPassword() {
+        const passwd = document.getElementById("password").value;
+        console.log(passwd);
+        checkPassword(passwd)
     }
-}
 
-function lostFocus() {
-    printPassword();
+    function checkPassword(password) {
+        if (password === null || typeof password === "undefined") {
+            alert("abgebrochen")
+        } else if (password === "") {
+            alert("leer");
+        } else if (password.length >= 8) {
+            alert("richtig");
+        } else {
+            alert("falsch")
+        }
+    }
+
+    function lostFocus() {
+        printPassword();
+    }
+
 }
