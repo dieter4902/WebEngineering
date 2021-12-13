549341c33a256cfe635df41ef4546739643d1400
adrian <s51037@bht-berlin.de>
2021-12-13 12:31:07 +0100

    DOM fertig

--- js/js2.js
+++ js/js2.js
@@ -1,11 +1,16 @@
 {
     "use strict";
-    
+
     let submitButton = document.querySelector("[type=submit]");
     submitButton.addEventListener("click", function (event) {
         event.preventDefault();
         send();
     }, false);
+
+    let passwordButton = document.getElementById("btn");
+    passwordButton.addEventListener("click", function () {
+        printPassword()
+    })
 
     let resetButton = document.querySelector("[type=reset]");
     resetButton.addEventListener("click", function (event) {
@@ -15,6 +20,7 @@
 
     function reset() {
         changeFormBackground("red");
+        clearPic();
     }
 
     function send() {
@@ -22,6 +28,10 @@
         printPassword();
     }
 
+    function clearPic() {
+        const pwPic = document.getElementById("passwordPicture");
+        pwPic.src = "";
+    }
 
     function printPassword() {
         const passwd = document.getElementById("password").value;
@@ -29,17 +39,21 @@
         const pwPic = document.getElementById("passwordPicture");
         console.log(passwd);
         /* idk vielleicht sagt sie in der Vorlesung, dass wir das aus nem Directory lesen sollen? */
-        var passwdPics = ['img/porpoises.jpg', 'img/chonk_chart.png'];
+        const passwdPics = ['img/porpoises.jpg', 'img/chonk_chart.png'];
         let message;
         if (passwd === null || typeof passwd === "undefined") {
             message = "abgebrochen";
-        } else if (passwd === "") {
-            message = "leer";
+            pwPic.src = "";
         } else if (passwd.length >= 8) {
             message = "richtig";
-            pwPic.src = passwdPics[Math.floor(Math.random()*passwdPics.length)];
+            pwPic.src = passwdPics[Math.floor(Math.random() * passwdPics.length)];
         } else {
-            message = "falsch";
+            if (passwd === "") {
+                message = "leer";
+            } else {
+                message = "falsch";
+            }
+            pwPic.src = "";
         }
         pwMessage.innerHTML = message;
         console.log(message)
