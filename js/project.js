eadbbc2ce6a8786eae2b284cfd3fbb7e33654c6a
s51052 <eric@gmail.com>
2022-02-03 15:16:40 +0100

    aaa

--- js/project.js
+++ js/project.js
@@ -9,106 +9,28 @@
 
     let registerButton = document.getElementById("register");
     let navTag = document.getElementById("nav");
-    toggle();
+    let search = document.getElementById("search");
+
 
     loginButton.addEventListener("click", function () {
         toggle();
+        loginButton.textContent = "Login";
     });
+
     registerButton.addEventListener("click", function () {
         toggle();
+        loginButton.textContent = "Back";
     });
 
     function toggle() {
         registerButton.hidden = !registerButton.hidden;
-        loginDiv.hidden = registerButton.hidden;
-        formDiv.hidden = !registerButton.hidden;
-        loginButton.hidden = !registerButton.hidden;
-        navTag.hidden = registerButton.hidden;
+        loginDiv.hidden = !loginDiv.hidden;
+        formDiv.hidden = !formDiv.hidden;
+        search.hidden = !search.hidden;
+        navTag.hidden = !navTag.hidden;
     }
 
     let allElements = document.querySelectorAll("input,textarea,select");
-    //printet alle Elemente
-    const printAllElements = () => {
-        console.log("All values:");
-        allElements.forEach(i => console.log(i.id + i + ": " + i.value));
-    }
-
-    //printet alle Elemente, welche einen value haben
-    const printValueElements = () => {
-        console.log("Filled values:");
-        allElements.forEach(i => {
-            if (i.value !== undefined && i.value !== "" && i.value != null) {
-                console.log(i.id + ": " + i.value);
-            }
-        });
-    };
-
-    let printValidValueElements = () => {
-        let check = true;
-        allElements.forEach(i => {
-            if (i.value !== undefined && i.value !== "" && i.value != null) {
-                if (i.validity.valid) {
-                    console.log(i.id + ": " + i.value);
-                } else if (i.validity.valueMissing) {
-                    console.log(i.id + ": " + "Bitte Feld ausfüllen!");
-                } else if (i.validity.tooShort) {
-                    console.log(i.id + ": " + "Mehr zeichen erforderlich!");
-                } else if (i.validity.rangeUnderflow) {
-                    console.log(i.id + ": " + "Du bist zu jung!");
-                }
-            } else {
-                if (i.type === "submit" || i.type === "reset") {
-                } else {
-                    console.log(i.id + ": " + "Falsche Eingabe!");
-                    check = false;
-                }
-            }
-        });
-        return check;
-    };
-    printAllElements();
-    printValueElements();
-
-
-    //guckt, ob das Passwort valid ist und printet es auf die Konsole, wenn ja
-    let passwordButton = document.getElementById("btn");
-    passwordButton.addEventListener("click", function () {
-        console.clear();
-        const passwd = document.getElementById("password");
-        if (passwd.validity.valid) {
-            console.log(passwd.value);
-        } else {
-            console.log("bad password");
-        }
-    });
-
-    let form = document.querySelector("form");
-
-    let createDOM = (ausgabe) => {
-        if (form.lastChild.id === "ausgabe") {
-            /*             let text = document.createTextNode("Richtig"); */
-            form.lastChild.lastChild.textContent = ausgabe;
-            /*   form.lastChild.appendChild(text); */
-        } else {
-            let message = document.createElement("p");
-            message.id = "ausgabe";
-            let text = document.createTextNode(ausgabe);
-            message.appendChild(text);
-            form.appendChild(message);
-        }
-    }
-
-    let clearForm = () => {
-        allElements.forEach(i => {
-            if (i.type === "submit" || i.type === "reset")
-                return; //reset und submit button sollen nicht geleert werden
-            if (i.type === "checkbox") {
-                i.checked = false;
-                return;
-            }
-            i.value = "";
-        });
-    }
 
     let submitButton = document.querySelector("[type=submit]");
     submitButton.addEventListener("click", function (event) {
